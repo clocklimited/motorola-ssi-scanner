80dc3c9dd673a97000e1efacb5de75603a90544b
Tom Gallacher <tomgallacher23@gmail.com>
2013-01-29 14:01:05 +0000

    Moving to a module to fit with the station API

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var serialport = require('serialport')
   , SerialPort = serialport.SerialPort
-  , scanner = new SerialPort('/dev/ttyACM0', {
+  , device = new SerialPort('/dev/ttyACM0', {
     baudrate: 9600
   })
   , commands = require('./lib/commands')
+  , EventEmitter = require('events').EventEmitter
 
 serialport.list(function (err, ports) {
   console.log('Available Ports: ')
@@ -12,14 +13,29 @@ serialport.list(function (err, ports) {
   })
 })
 
-scanner.on('open', function () {
-  scanner.on('data', function (data) {
-    console.log('recieved data: ')
-    console.log(data)
-    console.log('(ascii): ' + data.toString())
+module.exports = function() {
+  var scanner = device
+    , self = new EventEmitter()
+
+  scanner.on('open', function () {
+    scanner.on('data', function (data) {
+      self.emit('recieved',
+        { raw: data
+        , ascii: data.toString()
+        }
+      )
+    })
   })
-  setInterval(function() { commands.getCommand(0xe4, null, function(scan) {
-    console.log(scan)
-    scanner.write(scan)
-  })}, 10000)
-})
+
+  self.sendCommand = function (opCode, payload) {
+    commands.getCommand(opCode, payload, function(packet) {
+      scanner.write(packet)
+      self.emit('sent', opCode, payload)
+    })
+  }
+
+  self.serialport = scanner
+  self.isLocked = false
+
+  return self
+}
