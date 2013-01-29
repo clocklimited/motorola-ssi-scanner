765b2abe22f409c3689b2bec36b6995b031a7738
Tom Gallacher <tomgallacher23@gmail.com>
2013-01-29 15:01:03 +0000

    Correct initialisation

--- test/scan-qrcode-test.js
+++ test/scan-qrcode-test.js
@@ -1,7 +1,9 @@
-var motorolaSSI = require('../')
+var MotorolaSSI = require('../')
+  , scanner = new MotorolaSSI()
+setInterval(function() {
+  scanner.sendCommand(0xe4, null)
+}, 10000)
 
-setInterval(motorolaSSI.sendCommand(0xe4, null), 10000)
-
-motorolaSSI.on('data', function () {
+scanner.on('data', function () {
   console.log(arguments)
 })
