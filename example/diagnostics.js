e0f826751ebabe8cae11d12cdde95fc7b0808c0b
Paul Serby <paul.serby@clock.co.uk>
2015-08-10 16:01:11 +0100

    Improving linting and qa task

--- example/diagnostics.js
+++ example/diagnostics.js
@@ -6,8 +6,7 @@ scanner.start()
 
 scanner.on('ready', function() {
 
-	scanner._send(opcodes.ssiMgmtCommand, [ 0x00, 0x06, 0x20, 0x00, 0xff, 0xff ], function(err, data) {
-	})
+	scanner._send(opcodes.ssiMgmtCommand, [ 0x00, 0x06, 0x20, 0x00, 0xff, 0xff ], function() {})
 
 	setTimeout(function() {
 		scanner._send(opcodes.ssiMgmtCommand, [ 0x00, 0x08, 0x02, 0x00, 0x27, 0x4d, 0x42, 0x00 ], function(err, data) {
