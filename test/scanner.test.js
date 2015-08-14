5499b4be4e43367b026db0a394c739e2976514ab
Paul Serby <paul.serby@clock.co.uk>
2015-08-15 00:06:11 +0100

    Fixing _handleTest

--- test/scanner.test.js
+++ test/scanner.test.js
@@ -13,7 +13,7 @@ describe('scanner', function() {
 
 			var scanner = new Scanner({ port: '/foo', logger: logger })
 			scanner.device = { write: noop }
-			scanner._handleTransmission(4, function(data) {
+			scanner._handleTransmission(4, function(packet, data) {
 					assert.equal(data.toString('ascii'), 'http')
 					done()
 				})(new Buffer([ 0xf3, 0x00, 0x00, 0x1c, 0x68, 0x74, 0x74, 0x70 ]))
@@ -23,7 +23,7 @@ describe('scanner', function() {
 		it('should return data when it is the final packet', function(done) {
 
 			var scanner = new Scanner({ port: '/foo', logger: logger })
-				, fn = scanner._handleTransmission(4, function(data) {
+				, fn = scanner._handleTransmission(4, function(packet, data) {
 					assert.equal(data.toString('ascii'), 'httphttp')
 					done()
 				})
